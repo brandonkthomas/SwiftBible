f68bde70cc3dc4fa5eadaa77de0f048c323406fc
Brandon Thomas <bkt02@me.com>
2026-06-26 23:22:20 -0500

    theme prototype color tweaks

--- SwiftBible/App/RootTabView.swift
+++ SwiftBible/App/RootTabView.swift
@@ -13,8 +13,11 @@ struct RootTabView: View {
         case search
     }
 
+    private var themeDefault: Color = .init(.systemBackground)
     private var themeOffBlack: Color = .init(red: 0.075, green: 0.075, blue: 0.075)
+    private var themeBlack: Color = .init(.black)
     private var themeOffWhite: Color = .init(red: 0.925, green: 0.901, blue: 0.858)
+    private var themeWhite: Color = .init(.white)
 
     private var demoText: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac libero non ipsum convallis suscipit at a tortor. In ligula elit, rhoncus sit amet auctor id, ornare id purus. Nullam neque mauris, luctus euismod malesuada at, tempus et enim. Cras tempus efficitur mauris, non dapibus diam rutrum nec. Praesent sollicitudin massa et elementum efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus metus ipsum, pharetra sed urna et, facilisis maximus massa. Suspendisse potenti. Curabitur elementum tellus in nulla vulputate eleifend. Sed at pharetra nunc, sed tempus ligula. Vestibulum scelerisque ut enim vel malesuada. Mauris in est sem. Donec mollis dolor vitae mi gravida, eu feugiat est interdum. Sed eu elit a diam sodales ornare. Integer rhoncus, quam non eleifend lacinia, enim arcu interdum enim, vel cursus quam ante a magna. Mauris sodales mi ante, a suscipit enim egestas at."
 
@@ -65,7 +68,7 @@ struct RootTabView: View {
             // prevent tab bar from covering up bottom few lines
             .padding(EdgeInsets(top: 24, leading: 0, bottom: 75, trailing: 0))
         }
-        .background(themeOffBlack) // TODO: remove in favor of theme service
+        .background(themeDefault) // TODO: remove in favor of theme service
     }
 
     /// Saved tab
