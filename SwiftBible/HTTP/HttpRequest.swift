e30db1c5f909010f2756783c5df8e0fa98cb6f9c
Brandon Thomas <bkt02@me.com>
2026-06-28 20:45:19 -0500

    reduced icon shadows in dark mode

---
+++ SwiftBible/HTTP/HttpRequest.swift
@@ -0,0 +1,35 @@
+//
+//  HttpRequests.swift
+//  SwiftBible
+//
+//  Created by Brandon Thomas on 6/28/26.
+//
+
+import Foundation
+
+enum HttpRequest {
+
+    /// Performs a GET request for a specific
+    static func fetchData(url: URL,
+                          httpMethod: String,
+                          headers: [String: String],
+                          urlSession: URLSession) async throws -> Data {
+        // Configure request
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+
+        for header in headers {
+            request.setValue(header.value, forHTTPHeaderField: header.key)
+        }
+
+        // Perform the request
+        let (data, response) = try await urlSession.data(for: request)
+
+        // Check for a valid HTTP response
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+
+        return data
+    }
+}
