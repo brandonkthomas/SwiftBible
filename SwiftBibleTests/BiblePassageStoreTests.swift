105acc12cd9b08e44dcbc3d3e01ff447878b21ed
Brandon Thomas <bkt02@me.com>
2026-09-14 22:54:39 -0500

    implemented BiblePassageStore (centralized passage caching) + unit tests

---
+++ SwiftBibleTests/BiblePassageStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  BiblePassageStoreTests.swift
+//  SwiftBibleTests
+//
+//  Created by Brandon Thomas on 2026-09-13.
+//
+
+import Testing
+
+struct BiblePassageStoreTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
